199e4df2e53f5bc85c1426be11ea1e2203cd8a41
Oddvar Strømmen <oddvars@gmail.com>
2016-10-10 14:54:50 +0200

    added store and extra libs

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,10 @@
-{-# LANGUAGE DataKinds       #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE TypeOperators   #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TypeOperators     #-}
+{-# LANGUAGE RecordWildCards   #-}
+
 module Lib
     ( startApp
     ) where
@@ -9,16 +13,42 @@ import           Control.Concurrent.STM
 import           Control.Monad.Reader
 import qualified Database.EventStore as E
 import           Data.Aeson
+import           Data.Aeson.Types
 import           Data.Aeson.TH
+import           Data.Hashable
+import           Data.Text
+import           Data.Time
+import           Data.UUID
+import           Data.UUID.V4
+import           GHC.Generics
 import           Network.Wai
 import           Network.Wai.Handler.Warp
 import           Servant
+import           Store
 
+newtype CustomerId = CustomerId UUID deriving (Eq, Ord, Generic)
 
+instance Show CustomerId where
+  show (CustomerId uuid) = show uuid
+
+instance Hashable CustomerId where
+  hash (CustomerId uuid) = hash $ toASCIIBytes uuid
+
+instance ToJSON CustomerId where
+  toJSON (CustomerId uuid) = String $ pack $ toString uuid
+
+instance FromJSON CustomerId where
+  parseJSON (String str) =
+    case fromString $ unpack str of
+      Just uuid -> return $ CustomerId uuid
+      _         -> typeMismatch "Invalid CustomerId UUID " (String str)
+  parseJSON v = typeMismatch "Invalid CustomerId " v
+
+customerStream :: CustomerId -> Text
+customerStream (CustomerId uuid) = "policy:" E.<> pack (toString uuid)
 
 data AppConfig = AppConfig
-  { esConnection :: E.Connection
-  }
+  { esConnection :: E.Connection }
 
 data User = User
   { userId        :: Int
@@ -26,10 +56,28 @@ data User = User
   , userLastName  :: String
   } deriving (Eq, Show)
 
-data CustomerEvent
-  = CustomerCreated
-  | AddressUpdate
+data CustomerCommand
+  = CreateCustomer CustomerId Text Text
+  | MoveCustomer CustomerId Text
   deriving (Eq, Show)
+
+data CustomerEvent
+  = CustomerCreated CustomerId Text Text
+  | CustomerMoved
+  deriving (Eq, Show)
+
+data Customer
+  = Customer
+  { _conn :: E.Connection
+  , _id :: CustomerId
+  , _var :: TVar CustomerSnapshot
+  }
+
+data CustomerSnapshot
+  = CustomerSnapshot
+  { customerName :: Text
+  , customerAddress :: Text
+  }
 
 $(deriveJSON defaultOptions ''User)
 $(deriveJSON defaultOptions ''CustomerEvent)
@@ -42,17 +90,61 @@ readerToEither cfg = Nat $ \x -> runReaderT x cfg
 type API = "user" :> Get '[JSON] User
       :<|> "user" :> ReqBody '[JSON] User :> Post '[JSON] NoContent
 
+seed :: CustomerSnapshot
+seed =  CustomerSnapshot "" ""
+
+apply :: CustomerSnapshot -> CustomerCommand -> CustomerSnapshot
+apply s (CreateCustomer cid name address) = createCustomer s cid name address
+
+newCustomer :: E.Connection -> CustomerCommand -> IO (Maybe Customer)
+newCustomer _conn (CreateCustomer cid name address) = do
+  let cmd = CreateCustomer cid name address
+      evt = toEvent cmd
+      _id = cid
+  writeEvents <- E.sendEvent _conn (customerStream _id) E.noStreamVersion evt
+  atomically $ do
+    _ <- E.waitSTM writeEvents
+    let p = apply seed cmd
+    _var <- newTVar p
+    return $ Just Customer {..}
+newCustomer _conn _ = return Nothing
+
+buildCustomer :: E.Connection -> CustomerId -> IO Customer
+buildCustomer conn customerId = do
+  s   <- streamFold conn (customerStream customerId) customer seed
+  var <- newTVarIO s
+  return $ Customer conn customerId var
+  where
+    customer s (CustomerCreated cid name address ) _ = return $ createCustomer s cid name address
+
+createCustomer :: CustomerSnapshot -> CustomerId -> Text -> Text -> CustomerSnapshot
+createCustomer snap cid name address =
+  snap { customerName = name
+       , customerAddress = address }
+
+execute :: E.Connection -> CustomerId -> CustomerCommand -> IO ()
+execute _conn _id cmd = do
+  writeEvent <- E.sendEvent _conn (customerStream _id) E.anyVersion (toEvent cmd)
+  atomically $ do
+    _ <- E.waitSTM writeEvent
+    return ()
+
+toEvent :: CustomerCommand -> E.Event
+toEvent (CreateCustomer customerId name address) =
+  E.createEvent "customer-created" Nothing $
+    E.withJson (CustomerCreated customerId name address)
+
 startApp :: IO ()
 startApp = do
   conn <- E.connect E.defaultSettings (E.Static "127.0.0.1" 1113)
   let cfg = AppConfig {esConnection = conn}
-  run 8080 $ app cfg
-
-app :: AppConfig -> Application
-app cfg = serve api (apiServer cfg)
+  run 9000 $ app cfg
 
 api :: Proxy API
 api = Proxy
+
+app :: AppConfig -> Application
+app cfg = serve api (apiServer cfg)
 
 apiServer :: AppConfig -> Server API
 apiServer cfg = enter (readerToEither cfg) server
@@ -64,4 +156,10 @@ load :: ApiHandler User
 load = return $ User 1 "Isaac" "Newton"
 
 create :: User -> ApiHandler NoContent
-create u = return NoContent
+create u = do
+  cfg <- ask
+  uuid <- liftIO nextRandom
+  let customerId = CustomerId uuid
+      cmd = CreateCustomer customerId "Joe Average" "10 Good enough road"
+  _ <- liftIO $ execute (esConnection cfg) customerId cmd
+  return NoContent
